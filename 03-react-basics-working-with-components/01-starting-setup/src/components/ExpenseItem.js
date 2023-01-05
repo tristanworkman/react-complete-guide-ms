d5939d951159f8f2711712bfafc048dd327156de
Tristan Workman <sungjinworkman@gmail.com>
2023-01-05 08:30:29 -0600

    morning

--- 03-react-basics-working-with-components/01-starting-setup/src/components/ExpenseItem.js
+++ 03-react-basics-working-with-components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-function ExpenseItem() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  )
+import "./ExpenseItem.css";
+
+function ExpenseItem(prop) {
+	return (
+		<div className='expense-item'>
+			<div>{prop.date.toDateString()}</div>
+			<div className='expense-item__description'>
+				<h2>{prop.title}</h2>
+				<div className='expense-item__price'>${prop.amount}</div>
+			</div>
+		</div>
+	);
 }
 
-export default ExpenseItem
+export default ExpenseItem;
