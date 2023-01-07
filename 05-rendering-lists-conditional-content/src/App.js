616d07134a50353b47003f7f56cbf21faa20fdfa
Tristan Workman <sungjinworkman@gmail.com>
2023-01-07 10:53:47 -0600

    key and map

--- 05-rendering-lists-conditional-content/src/App.js
+++ 05-rendering-lists-conditional-content/src/App.js
@@ -1,43 +1,43 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const dummyExpenses = [
+	{
+		id: "e1",
+		title: "Toilet Paper",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: "e4",
+		title: "New Desk (Wooden)",
+		amount: 450,
+		date: new Date(2021, 5, 12),
+	},
+];
+
 const App = () => {
-	const dummyExpenses = [
-		{
-			id: "e1",
-			title: "Toilet Paper",
-			amount: 94.12,
-			date: new Date(2020, 7, 14),
-		},
-		{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-		{
-			id: "e3",
-			title: "Car Insurance",
-			amount: 294.67,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: "e4",
-			title: "New Desk (Wooden)",
-			amount: 450,
-			date: new Date(2021, 5, 12),
-		},
-	];
-	// const [totalExpenses, setTotalExpenses] = useState(dummyExpenses);
+	const [expenses, setExpenses] = useState(dummyExpenses);
 
 	const addExpenseHandler = (expense) => {
-		// setTotalExpenses((prevState) => {
-		// 	return [...prevState, expense];
-		// });
-		console.log(expense);
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
+		// console.log(expense);
 	};
-	// console.log(totalExpenses);
 
 	return (
 		<div>
 			<NewExpense onAddExpense={addExpenseHandler} />
-			<Expenses items={dummyExpenses} />
+			<Expenses items={expenses} />
 		</div>
 	);
 };
