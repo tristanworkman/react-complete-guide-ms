17ee91f342141aff13281ef15abd90fd30fe6cab
Tristan Workman <sungjinworkman@gmail.com>
2023-01-07 17:35:12 -0600

    ExpensesList

--- 05-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
+++ 05-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -15,26 +15,13 @@ const Expenses = (props) => {
 		(expense) => expense.date.getFullYear().toString() === filteredYear
 	);
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<Card className='expenses'>
 			<ExpensesFilter
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{expensesContent}
+			<ExpensesList items={filteredExpenses}></ExpensesList>
 		</Card>
 	);
 };
