fb6ce274641ae8887b3d15052e31a83f3d7cc130
Tristan Workman <sungjinworkman@gmail.com>
2023-01-07 16:55:21 -0600

    red

--- 05-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
+++ 05-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
@@ -12,22 +12,29 @@ const Expenses = (props) => {
 	};
 
 	const filteredExpenses = props.items.filter(
-		(item) => filteredYear === item.date.getFullYear().toString()
+		(expense) => expense.date.getFullYear().toString() === filteredYear
 	);
+
+	let expensesContent = <p>No expenses found.</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<Card className='expenses'>
 			<ExpensesFilter
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{filteredExpenses.map((expense) => (
-				<ExpenseItem
-					key={expense.id}
-					title={expense.title}
-					amount={expense.amount}
-					date={expense.date}
-				/>
-			))}
+			{expensesContent}
 		</Card>
 	);
 };
