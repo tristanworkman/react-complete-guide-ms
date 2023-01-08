b692770e9cc8441c69a95c44461f92705354dc61
Tristan Workman <sungjinworkman@gmail.com>
2023-01-08 12:43:13 -0600

    Assignment 4

--- 05-rendering-lists-conditional-content/src/components/NewExpense/NewExpense.js
+++ 05-rendering-lists-conditional-content/src/components/NewExpense/NewExpense.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
@@ -8,15 +9,34 @@ const NewExpense = (props) => {
 			...enteredExpenseData,
 		};
 		props.onAddExpense(expenseData);
+		setIsEditing(false);
+	};
+
+	const [isEditing, setIsEditing] = useState(false);
+
+	const startEditingHandler = () => {
+		setIsEditing(true);
+	};
+
+	const stopEditingHandler = () => {
+		setIsEditing(false);
 	};
 
 	return (
 		<div className='new-expense'>
-			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+			{!isEditing && (
+				<button onClick={startEditingHandler}>Add New Expense</button>
+			)}
+			{isEditing && (
+				<ExpenseForm
+					onCancel={stopEditingHandler}
+					onSaveExpenseData={saveExpenseDataHandler}
+				/>
+			)}
 		</div>
 	);
 };
 
 export default NewExpense;
 
-//useState receives an (optional) initial state value as an argument. It also returns an array with exactly two elements. It does not update the state value when you call useState again. Calling useState will simply create a new State. If you update state that depends on the previous state, you should use the 'function form' of the state updating function instead.
+//useState receives an (optional) initial state value as an argument. It also returns an array with exactly two elements. It does not update the state value when you call useState again. Calling useState will simply create a new State. If you update state that depends on the previous state, you should use the 'function form' of the state updating function instead. The && in conditionals is a shortcut and is the exact as the "?".
