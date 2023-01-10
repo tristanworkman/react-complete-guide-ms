48e51c04ec5b3a430c70409ca1a9b466aa5364b3
Tristan Workman <sungjinworkman@gmail.com>
2023-01-09 21:22:53 -0600

    styled

--- 06-styling-react-components/src/components/UI/Button/Button.js
+++ 06-styling-react-components/src/components/UI/Button/Button.js
@@ -1,6 +1,25 @@
-import React from "react";
+// import styled from 'styled-components'
 
 import "./Button.css";
+
+// font: inherit;
+// padding: 0.5rem 1.5rem;
+// border: 1px solid #062dad;
+// color: white;
+// background: #062dad;
+// box-shadow: 0 3px 10px rgb(63, 29, 255);
+// cursor: pointer;
+
+// &:focus {
+// outline: none;
+// }
+
+// &:hover,
+// &:active {
+// background: #0e60ac;
+// border-color: #0e60ac;
+// box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
 
 const Button = (props) => {
 	return (
