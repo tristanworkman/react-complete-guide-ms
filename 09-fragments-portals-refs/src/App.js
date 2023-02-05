da7f707fc871a8b42f2efe0a153beddfe8616f73
Tristan Workman <sungjinworkman@gmail.com>
2023-02-05 15:55:12 -0600

    100-106

--- 09-fragments-portals-refs/src/App.js
+++ 09-fragments-portals-refs/src/App.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+// import Wrappers from "./Helpers/Wrappers";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+	const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+	const addUserHandler = (uName, uAge) => {
+		setUsersList((prevUsersList) => {
+			return [
+				...prevUsersList,
+				{ name: uName, age: uAge, id: Math.random().toString() },
+			];
+		});
+	};
 
-  return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
-  );
+	return (
+		<>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</>
+	);
 }
 
 export default App;
